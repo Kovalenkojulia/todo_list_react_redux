8daf5b7457d6610f633fb33b0e8a080d4c2c6b1a
Kovalenkojulia <webstudiomosaic@gmail.com>
2022-06-13 19:39:22 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { addTodo } from "./store/todoSlice";
+
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+
+  const addTask = () => {
+    dispatch(addTodo({ text }));
+    setText("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputField text={text} handleSubmit={addTask} handleInput={setText} />
+      <TodoList />
     </div>
   );
 }
